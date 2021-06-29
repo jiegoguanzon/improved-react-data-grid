77aae5fe3fc9b59cb4c2a86f9b1edb8f25e9abd7
Jiego Guanzon <jiego_benjamin.guanzon@bp.com>
2021-06-29 10:28:11 +0800

    Cleanup files

--- lib/utils/domUtils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function stopPropagation(event: React.SyntheticEvent): void;
-export declare function wrapEvent<E extends React.SyntheticEvent>(ourHandler: React.EventHandler<E>, theirHandler: React.EventHandler<E> | undefined): (event: E) => void;
